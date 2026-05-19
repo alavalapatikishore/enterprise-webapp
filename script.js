de87ee4005938f4a45101347ae7091bc2e3018ef
Your Name <your-email@example.com>
2026-05-19 11:46:43 +0530

    text chagne

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showMessage() {
   document.getElementById('message').textContent =
-    'Application is running successfully! with docker image';
+    'Application is running successfully!';
 }
